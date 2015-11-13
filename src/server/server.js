809955d77ddf25e3217386ac3e57f7f18b850f4a
Koby Boyango <koby.b@mce-sys.com>
2015-11-13 17:03:12 +0200

    server: * Don't run the webpack server on production * Use process.env.PORT when set

--- src/server/server.js
+++ src/server/server.js
@@ -1,34 +1,37 @@
 var express = require('express');
 var path = require('path');
 
+var isDevelopment = (process.env.NODE_ENV !== 'production');
+
 var app = express();
 
 var distPath = path.resolve(__dirname, '../../dist');
 app.use(express.static(distPath));
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
   console.log('Server is listening at http://%s:%s', host, port);
 });
 
+if (isDevelopment) {
+    // Webpack dev
+    var webpack = require('webpack');
+    var WebpackDevServer = require('webpack-dev-server');
+    var config = require('../../config/webpack.js');
 
-// Webpack dev
-var webpack = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
-var config = require('../../config/webpack.js');
- 
-new WebpackDevServer(webpack(config), {
-   hot: true,
-   historyApiFallback: true,
-   proxy: {
-     '*': 'http://localhost:3000'
-   }
-}).listen(3001, 'localhost', function (err, result) {
-   if (err) {
-     console.log(err);
-   }
+    new WebpackDevServer(webpack(config), {
+        hot: true,
+        historyApiFallback: true,
+        proxy: {
+            '*': 'http://localhost:3000'
+        }
+    }).listen(3001, 'localhost', function (err, result) {
+        if (err) {
+            console.log(err);
+        }
 
-   console.log('Webpack dev server is listening at localhost:3001');
-});
+        console.log('Webpack dev server is listening at localhost:3001');
+    });
+}
