e8d6f59ec63664d044c71831dbef8c1d687cecee
Koby Boyango <koby.b@mce-sys.com>
2015-11-14 11:47:37 +0200

    * Use  material design lite (react-mdl) * Updated login screen

--- src/client/js/components/Login.jsx
+++ src/client/js/components/Login.jsx
@@ -1,20 +1,33 @@
 import React from 'react';
+import { Button, Textfield } from 'react-mdl';
+
 import "../../css/login.css";
 
 export default class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            userName: ""
+        }
+    }
     render() {
         return (
             <div className="login">
-                <input type='text' ref='userName' />
-                <button onClick={(e) => this.handleClick(e)}>
-                    Login
-                </button>
+                <div className="login-item-container">
+                    <Textfield
+                        type="text"
+                        label="Please enter you name..."
+                        onChange={ (e) => this.setState({userName: e.target.value})}
+                    />
+                </div>
+                <div className="login-item-container">
+                    <Button
+                        ripple={true}
+                        onClick={(e) => this.props.onLogin(this.state.userName)}>
+                            Login
+                    </Button>
+                </div>
             </div>
         )
-    }
-
-    handleClick(e) {
-        const userName = this.refs.userName.value.trim();
-        this.props.onLogin(userName);
     }
 }
--- src/client/js/index.jsx
+++ src/client/js/index.jsx
@@ -7,6 +7,9 @@ import App from './containers/App.jsx'
 import chatReducer from './reducers/index.jsx'
 import { renderDevTools, createDevStore } from './utils/DevTools.js'
 
+import 'react-mdl/extra/material'
+import 'react-mdl/extra/material.css'
+
 let finalCreateStore = __DEV__ ? createDevStore : createStore;
 
 const store = finalCreateStore(chatReducer);
