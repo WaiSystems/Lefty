b09b6bcec691576c9009597818286b072081c78f
Koby Boyango <koby.b@mce-sys.com>
2015-11-27 17:59:42 +0200

    client: Set the page's title and update it when the user logs in\out

--- src/client/js/index.jsx
+++ src/client/js/index.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux'
+import * as _ from 'lodash';
 
 import 'react-mdl/extra/material'
 import 'react-mdl/extra/material.css'
@@ -13,6 +14,17 @@ import { renderDevTools } from './utils/DevTools.js'
 import {configureStore} from './store'
 
 const store = configureStore();
+
+// Update the page's title when the user logs in\out
+store.subscribe(() => {
+    const userDisplayName = _.get(store.getState(), 'userData.self.displayName', null);
+    //console.log(JSON.stringify(store));
+    if (userDisplayName) {
+        document.title = 'Lefty - ' + userDisplayName;
+    } else {
+        document.title = 'Lefty';
+    }
+});
 
 ReactDOM.render(
     <div>
